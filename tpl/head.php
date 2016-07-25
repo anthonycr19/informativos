435ff9ea5cf8235c3427964d2d4bfddddde18806
Edwin Deza Culque <Edwin Deza Culque>
2016-07-24 22:49:08 -0500

    tercera actualización

---
+++ tpl/head.php
@@ -0,0 +1,17 @@
+  <!-- Documento para datos de cabecera -->
+
+  <meta charset="UTF-8">
+  <meta lang="ES">
+  <meta name="keywords" content="" > 
+  <link rel="shortcut icon" href="./app/img/favicon.png" type="image/x-icon">
+  <meta name="description" content="" >
+  <title>MI EMPRESA</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300,100,700,900' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="app/css/fancybox.css"/>
+  <link rel="stylesheet" href="app/css/bootstrap.css"/>
+  <link rel="stylesheet" href="app/css/style.css"/>
+  <script src="app/js/jquery.js"></script>
+  <script src="app/js/fancybox.js"></script>
+  <script src="app/js/bootstrap.js"></script>
+  <script src="app/js/app.js"></script>
