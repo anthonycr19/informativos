5a127662e35e1024f4ce89d927c7d40ca6f84833
Edwin Deza Culque <Edwin Deza Culque>
2016-07-24 22:08:41 -0500

    Actualización vista 01 y arquitectura

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include 'tpl/head.php'; ?>
+</head>
+<body>
+<?php include 'tpl/header.php'; ?>
+
+<div class="section_home">
+	<div class="container">
+		<h2>Bienvenido al Sistema de Control de Personal</h2>
+		<p>
+			En este sistema se podrán ver y generar documentos de los reportes brindados diariamente por SCP(Sistema de Control de Personal).
+			<br><br>
+			Recordatorío: <br>
+		</p>
+
+	</div>
+</div>
+
+<?php include 'tpl/footer.php'; ?>
+</body>
+</html>
---
+++ tpl/header.php
@@ -0,0 +1,24 @@
+<header>
+	<nav class="navbar navbar-default container">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+		 <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="inicio.php">Inicio <span class="glyphicon glyphicon-home"></span></a></li>
+        <li><a href="dashboard.php">Dashboard <span class="glyphicon glyphicon-stats"></span></a></li>
+        <li><a href="reportes.php">Reportes <span class="glyphicon glyphicon-credit-card"></span></a></li>
+      </ul>
+      <h1>Sistema de Control de Personal</h1>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</header>
